bb1d257b7965b995b71c839cb3ea9255cb36affb
Ritik Kumar Pathak <ritik.p@awesomesuite.com>
2024-01-21 14:25:24 +0530

    c

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const cookieParser = require("cookie-parser");
 // Middlewares
 // app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.raw({limit:"50mb",type:"image/*"}))
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(express.raw({limit:"50mb",type:"image/*"}))
+app.use(bodyParser.urlencoded({ extended: true ,limit:'50mb'}));
 app.use(cookieParser());
 app.use(
   cors({
