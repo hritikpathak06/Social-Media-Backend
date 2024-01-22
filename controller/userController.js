0c79839aa1ba5e13f5ae2dd4736d197f9a48f412
Ritik Kumar Pathak <ritik.p@awesomesuite.com>
2024-01-22 11:13:57 +0530

    logout-issue-resolved

--- controller/userController.js
+++ controller/userController.js
@@ -96,7 +96,7 @@ exports.logout = async (req, res) => {
   res
     .status(200)
     .cookie("token", null, {
-      expires: new Date(Date.now()),
+      expires: new Date(0),
       httpOnly: true,
       secure: true,
       sameSite: "none",
